c5337be14a2d52e906ffc1ebe2b441d03b425fc4
XQ <XQ_MacBookPro@xq-MacBook-Pro.local>
2017-03-31 16:08:28 +0800

    初始化

---
+++ test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by XQ on 2017/3/31.
+//  Copyright © 2017年 xq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test2
+//
+//  Created by XQ on 2017/3/31.
+//  Copyright © 2017年 xq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
